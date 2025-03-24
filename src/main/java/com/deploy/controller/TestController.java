ea40cddf78c4c67eb91a5befff4fb6c0e17663c6
Jinyoung <glitt5384@naver.com>
2025-03-24 23:27:20 +0900

    TEST: 컨트롤러 어노테이션 추가

--- src/main/java/com/deploy/controller/TestController.java
+++ src/main/java/com/deploy/controller/TestController.java
@@ -2,7 +2,9 @@ package com.deploy.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController {
     @Value("${instance.name:unknown-instance}")
     private String instanceName;
